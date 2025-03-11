54143298ec670036c9873456f9e160cca84bd294
Piotr Benetkiewicz <pbenetkiewicz@gmail.com>
2025-03-11 17:10:23 +0100

    migrate to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
-import vercel from '@astrojs/vercel';
-
 import auth from 'auth-astro';
 
 import db from '@astrojs/db';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [preact(), auth(), db()],
-  adapter: vercel()
+  adapter: netlify(),
 });
